2ba691de0dfdb95bd4aad67ddd2155d25da14440
aehrlich1 <anatol.ehrlich@gmail.com>
2025-11-04 08:42:35 +0000

    Fix import path for GraphNmrDataset and add graph_nmr.py module with dataset processing functions

--- dgmc/datasets/__init__.py
+++ dgmc/datasets/__init__.py
@@ -1,4 +1,4 @@
-from .nmr import GraphNmrDataset, PairData, assign_numbers, generate_y_t, pre_transform_to_ones
+from .graph_nmr import GraphNmrDataset, PairData, assign_numbers, generate_y_t, pre_transform_to_ones
 from .random import RandomGraphDataset
 
 __all__ = [
